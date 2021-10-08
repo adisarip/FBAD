75ed141839bfac2d7df70378c71dc6398b4a6d14
tkhabia <tanmay.khabia@students.iiit.ac.in>
2021-10-08 18:05:50 +0530

    added code for Q5

---
+++ Assignmnent_1/tanmay/host.cpp
@@ -0,0 +1,152 @@
+#include <vector>
+#include <random>
+#include "xcl2.hpp"
+#include <algorithm>
+// #include <cstring>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    if (argc < 2 || argc > 4)
+    {
+        cout << "Incorrect format" << endl;
+        return EXIT_FAILURE;
+    }
+    char *binaryFile = argv[1];
+    int N  , Q;
+    if (argc == 3)
+    {
+        try
+        {
+            N = stoi(argv[2]);
+        }
+        catch (invalid_argument val)
+        {
+            cerr << "Invalid argument" << endl;
+        }
+    }
+    else
+    {
+        N = 4096;
+    }
+    if (argc == 4){
+        try 
+        {
+            Q = stoi(argv[2]);
+        }
+        catch (invalid_argument val)
+        {
+            cerr << "Invalid argument" << endl;
+        }
+    }
+    else
+    {
+        Q = 32;
+    }
+    vector<float, aligned_allocator<float>> data(N * 256);
+    vector<float, aligned_allocator<float>> query(Q * 256);
+    
+    vector<int, aligned_allocator<int>> res(Q);
+
+    cl::Device device;
+    cl::Context context;
+    cl::CommandQueue q;
+    cl::Program program;
+    cl::Kernel ;
+    cl_int err;
+
+    auto devices = xcl::get_xil_devices();
+
+    auto fileBuf = xcl::read_binary_file(binaryFile);
+    cl::Program::Binaries bins{{fileBuf.data(), fileBuf.size()}};
+    bool valid_device = false;
+    for (unsigned int i = 0; i < devices.size(); i++)
+    {
+        device = devices[i];
+        // Creating Context and Command Queue for selected Device
+        OCL_CHECK(err, context = cl::Context(device, NULL, NULL, NULL, &err));
+        OCL_CHECK(err,
+                  q = cl::CommandQueue(context, device,
+                                       CL_QUEUE_PROFILING_ENABLE |
+                                           CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE,
+                                       &err));
+        std::cout << "Trying to program device[" << i
+                  << "]: " << device.getInfo<CL_DEVICE_NAME>() << std::endl;
+        cl::Program program(context, {device}, bins, NULL, &err);
+        if (err != CL_SUCCESS)
+        {
+            std::cout << "Failed to program device[" << i << "] with xclbin file!\n";
+        }
+        else
+        {
+            std::cout << "Device[" << i << "]: program successful!\n";
+            // Creating Kernel
+            OCL_CHECK(err, krnl_fir = cl::Kernel(program, "matrix_multiply", &err));
+            valid_device = true;
+            break; // we break because we found a valid device
+        }
+    }
+
+    // Filling the data
+    float temp_sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        temp_sum = 0 ; 
+        for (int j = 0; j < 256; j++)
+        {
+            data[i * 256 + j] = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+            temp_sum += data[i * 256 + j] * data[i * 256 + j];
+        }
+        temp_sum = sqrt(temp_sum);
+        for (int j = 0; j < 256; j++)
+        {
+            data[i * 256 + j] /= temp_sum;
+        }
+    }
+    
+    for (int i = 0; i < Q; i++)
+    {
+        temp_sum = 0 ; 
+        for (int j = 0; j < 256; j++)
+        {
+            query[i * 256 + j] = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+            temp_sum +=query[i * 256 + j]*query[i * 256 + j];
+        }
+        temp_sum = sqrt(temp_sum);
+        for (int j = 0; j < 256; j++)
+        {
+            data[i * 256 + j] /= temp_sum;
+        }
+        
+    }
+
+    OCL_CHECK(err, cl::Buffer buffer_input1(context,
+                                           CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+                                           N*256*sizeof(float), data.data(), &err));
+    
+    OCL_CHECK(err, cl::Buffer buffer_input2 (context,
+                                           CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+                                           Q * 256 * sizeof(float), query.data(), &err));
+    
+    OCL_CHECK(err, cl::Buffer buffer_output (context,
+                                            CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
+                                            Q*sizeof(int), res.data(), &err));
+    OCL_CHECK(err, err = krnl_fir.setArg( 0 ,buffer_input1 ) ) ; 
+    OCL_CHECK(err, err = krnl_fir.setArg(1 , buffer_input2 ) ; 
+    OCL_CHECK(err , err = krnl_fir.setArg(2 , N) ) ; 
+    OCL_CHECK(err , err = krnl_fir.setArg(3 , Q) ) ; 
+    OCL_CHECK(err , err = krnl_fir.setArg(4 , buffer_output)) ; 
+    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_input1, buffer_input2}, 0 /* 0 means from host*/));
+    OCL_CHECK(err, err = q.finish());
+    // Launching the Kernels
+    OCL_CHECK(err, err = q.enqueueTask(krnl_fir));
+    // wait for the kernel to finish their operations
+    OCL_CHECK(err, err = q.finish());
+    
+    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_output},CL_MIGRATE_MEM_OBJECT_HOST));
+    OCL_CHECK(err, err = q.finish());
+    return 0  ; 
+}
---
+++ Assignmnent_1/tanmay/kernel.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+
+extern "C"{
+    void matrix_multiply(float *data  , float *query , int N , int  Q , int *res ){
+#pragma HLS INTERFACE m_axi port = data max_read_burst_length = 32  offset = slave bundle = gmem
+#pragma HLS INTERFACE m_axi port = query max_read_burst_length = 32  offset = slave bundle = gmem1
+#pragma HLS INTERFACE m_axi port = res max_write_burst_length = 32 offset = slave bundle = gmem2
+#pragma HLS INTERFACE s_axilite port = data bundle = control
+#pragma HLS INTERFACE s_axilite port = query bundle = control
+#pragma HLS INTERFACE s_axilite port = res bundle = control
+#pragma HLS INTERFACE s_axilite port = N bundle = control
+#pragma HLS INTERFACE s_axilite port = Q bundle = control
+#pragma HLS INTERFACE s_axilite port = return bundle = control
+        float query_local [1024]; // 4 queries 
+        float query_cal[4] , query_a_val[4] ;
+        int query_ans[4]; 
+        float data_val[256]; 
+        int n = N  , q  ; 
+        
+        for (int i = 0 ; i < Q +3 ; i+= 4){
+
+            if( Q < i +  4){
+                q = Q;
+            }else {
+                q = i + 4 ; 
+            }
+            ReadQuery: for (int j = i*256  ;j < q*256 ; j++){
+                #pragma HLS LOOP_TRIPCOUNT min=256 max = 1024 
+                #pragma HLS pipeline
+                #pragma HLS unroll factor=16
+                query_local[j -i]  = query [j];
+            }
+            INI:for(int j = 0 ; j < q-i ; j++){
+                #pragma HLS unroll factor=4
+                query_a_val[j]= 0;
+            }
+            Main:for(int j = 0  ; j< n ; j++){
+                #pragma HLS DATAFLOW
+                #pragma HLS stream variable=data_val depth=256
+                
+                query_cal[0] = 0 ;
+                query_cal[1] = 0 ;
+                query_cal[2] = 0 ;
+                query_cal[3] = 0 ;
+
+                M_rd:for (int k = 0; k < 256; k++)
+                {
+                    #pragma HLS pipeline
+                    data_val[k] = data[k + j*256];
+                }
+
+                M_cp:for (int k = 0; k < 256; k++)
+                {
+                    #pragma HLS pipeline
+                    for (int  l = 0; l < q-i; l++)
+                    {
+                        #pragma HLS LOOP_TRIPCOUNT min=1 max=4
+                        #pragma HLS unroll factor=4
+                        query_cal[l] += data_val[k] * query_local[l*256+k];
+                    }
+                }
+
+                M_uans:for (int k = 0; k < q-i; k++)
+                {
+                    #pragma HLS LOOP_TRIPCOUNT min=1 max=4
+                    #pragma HLS unroll factor=4
+                    if(query_cal[k] > query_a_val[k]){
+                        query_a_val[k] = query_cal[k];
+                        query_ans[k] = j;
+                    }
+                }
+            } 
+            Write_ans:for (int j = i; j < q; j++)
+            {
+                #pragma HLS unroll factor=4
+                res[j]= query_ans[j]; 
+            }
+        }
+    }
+}
