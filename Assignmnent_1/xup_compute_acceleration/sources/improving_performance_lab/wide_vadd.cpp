40066367624d4e5d308d5da9a75f4be59254db6e
tkhabia <tanmay.khabia@students.iiit.ac.in>
2021-10-09 16:50:20 +0530

    changed wide add for q4

--- Assignmnent_1/xup_compute_acceleration/sources/improving_performance_lab/wide_vadd.cpp
+++ Assignmnent_1/xup_compute_acceleration/sources/improving_performance_lab/wide_vadd.cpp
@@ -37,28 +37,119 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // Xilinx OpenCL and XRT includes
 #include "xilinx_ocl.hpp"
 
+#define NUM_BUFS 10
+
 void vadd_sw(uint32_t *a, uint32_t *b, uint32_t *c, uint32_t size)
 {
-    for (uint32_t i = 0; i < size; i++) {
+#pragma omp parallel for
+    for (int i = 0; i < size; i++) {
         c[i] = a[i] + b[i];
     }
 }
+
+int subdivide_buffer(std::vector<cl::Buffer> &divided, cl::Buffer buf_in, cl_mem_flags flags, int num_divisions)
+{
+    // Get the size of the buffer
+    size_t size;
+    size = buf_in.getInfo<CL_MEM_SIZE>();
+
+    if (size / num_divisions <= 4096) {
+        return -1;
+    }
+
+    cl_buffer_region region;
+
+    int err;
+    region.origin = 0;
+    region.size   = size / num_divisions;
+
+    // Round region size up to nearest 4k for efficient burst behavior
+    if (region.size % 4096 != 0) {
+        region.size += (4096 - (region.size % 4096));
+    }
+
+    for (int i = 0; i < num_divisions; i++) {
+        if (i == num_divisions - 1) {
+            if ((region.origin + region.size) > size) {
+                region.size = size - region.origin;
+            }
+        }
+        cl::Buffer buf = buf_in.createSubBuffer(flags, CL_BUFFER_CREATE_TYPE_REGION, &region, &err);
+        if (err != CL_SUCCESS) {
+            return err;
+        }
+        divided.push_back(buf);
+        region.origin += region.size;
+    }
+
+    return 0;
+}
+
+int enqueue_subbuf_vadd(cl::CommandQueue &q, cl::Kernel &krnl, cl::Event &event, cl::Buffer a, cl::Buffer b, cl::Buffer c)
+{
+    // Get the size of the buffer
+    cl::Event k_event, m_event;
+    std::vector<cl::Event> krnl_events;
+
+    static std::vector<cl::Event> tx_events, rx_events;
+
+    std::vector<cl::Memory> c_vec;
+    size_t size;
+    size = a.getInfo<CL_MEM_SIZE>();
+
+    std::vector<cl::Memory> in_vec;
+    in_vec.push_back(a);
+    in_vec.push_back(b);
+    q.enqueueMigrateMemObjects(in_vec, 0, &tx_events, &m_event);
+    krnl_events.push_back(m_event);
+    tx_events.push_back(m_event);
+    if (tx_events.size() > 1) {
+        tx_events[0] = tx_events[1];
+        tx_events.pop_back();
+    }
+
+    krnl.setArg(0, a);
+    krnl.setArg(1, b);
+    krnl.setArg(2, c);
+    krnl.setArg(3, (uint32_t)(size / sizeof(uint32_t)));
+
+    q.enqueueTask(krnl, &krnl_events, &k_event);
+    krnl_events.push_back(k_event);
+    if (rx_events.size() == 1) {
+        krnl_events.push_back(rx_events[0]);
+        rx_events.pop_back();
+    }
+    c_vec.push_back(c);
+    q.enqueueMigrateMemObjects(c_vec, CL_MIGRATE_MEM_OBJECT_HOST, &krnl_events, &event);
+    rx_events.push_back(event);
+
+    return 0;
+}
+
 
 int main(int argc, char *argv[])
 {
     // Initialize an event timer we'll use for monitoring the application
     EventTimer et;
     // Check if the binary file is passed as argument
-    if (argc != 2) {
-      std::cout << "Usage: " << argv[0] << " <XCLBIN File>" << std::endl;
-      return EXIT_FAILURE;
-    }
-    // Copy binary name
-    char* binaryName = argv[1];
-    // Get target and set BUFSIZE 1024 times bigger for hw runs
-    std::string target = getenv("XCL_EMULATION_MODE");
-    const uint32_t BUFSIZE = (target.compare("hw") == 0) ? (1024 * 1024 * 32) : (1024 * 32);
-
+    if (argc < 2 || argc > 4)
+        {
+            std::cout << "Usage: " << argv[0] << " <XCLBIN File>" << std::endl;
+            return EXIT_FAILURE;
+        }
+        // Copy binary name
+        char *binaryName = argv[1];
+        // Get target and set BUFSIZE 1024 times bigger for hw runs
+        uint32_t BUFSIZE;
+        if (argc == 3)
+        {
+            BUFSIZE = std::stoi(argv[2]);
+        }
+        else
+        {
+            char *target = getenv("XCL_EMULATION_MODE");
+            BUFSIZE = (strcmp( target, "hw") == 0) ? (1024 * 1024 * 32) : (1024 * 32);
+        }
     std::cout << "-- Parallelizing the Data Path --" << std::endl << std::endl;
 
     // Initialize the runtime (including a command queue) and load the
@@ -149,25 +240,24 @@ int main(int argc, char *argv[])
     vadd_sw(a, b, d, BUFSIZE);
     et.finish();
 
-    // Send the buffers down to the Alveo card
     et.add("Memory object migration enqueue");
-    cl::Event event_sp;
-    q.enqueueMigrateMemObjects({a_buf, b_buf}, 0, NULL, &event_sp);
-    clWaitForEvents(1, (const cl_event *)&event_sp);
+    q.enqueueUnmapMemObject(a_buf, a);
+    q.enqueueUnmapMemObject(b_buf, b);
 
-    et.add("OCL Enqueue task");
+    std::vector<cl::Buffer> a_bufs, b_bufs, c_bufs;
+    subdivide_buffer(a_bufs, a_buf, CL_MEM_READ_ONLY, NUM_BUFS);
+    subdivide_buffer(b_bufs, b_buf, CL_MEM_READ_ONLY, NUM_BUFS);
+    subdivide_buffer(c_bufs, c_buf, CL_MEM_WRITE_ONLY, NUM_BUFS);
 
-    q.enqueueTask(krnl, NULL, &event_sp);
-    et.add("Wait for kernel to complete");
-    clWaitForEvents(1, (const cl_event *)&event_sp);
+    std::array<cl::Event, NUM_BUFS> kernel_events;
 
-    // Migrate memory back from device
-    et.add("Read back computation results");
-    uint32_t *c = (uint32_t *)q.enqueueMapBuffer(c_buf,
-                                                 CL_TRUE,
-                                                 CL_MAP_READ,
-                                                 0,
-                                                 BUFSIZE * sizeof(uint32_t));
+    for (int i = 0; i < NUM_BUFS; i++) {
+        enqueue_subbuf_vadd(q, krnl, kernel_events[i], a_bufs[i], b_bufs[i], c_bufs[i]);
+    }
+
+    et.add("Wait for kernels to complete");
+    clWaitForEvents(NUM_BUFS, (const cl_event *)&kernel_events);
+
     et.finish();
 
 
